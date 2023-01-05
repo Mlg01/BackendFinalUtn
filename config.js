83e8c01665b5e95f94ec7713bbd86867b9086719
Matías <laudoniomatias@gmail.com>
2023-01-05 20:05:53 -0300

    Deploy

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 export const PORT = 4000;
-module.exports = {
-
-    DB_HOST: process.env.DB_HOST,
-    DB_PASSWORD : process.env.DB_PASSWORD,
-    DB_USER : process.env.DB_USER,
-    DB_DATABASE : process.env.DB_DATABASE,
-    DB_PORT : process.env.DB_PORT,
-    }
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_USER = process.env.DB_USER;
+export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_PORT = process.env.DB_PORT;
